7d6d67ac5ba6e24e8e3d3f08cba4cbff2e523af7
hjg212 <hjg212@exeter.ac.uk>
2016-11-15 14:34:38 +0000

    Added a line a white space in between each output

--- sumnum.c
+++ sumnum.c
@@ -3,7 +3,7 @@ int main()
 {
     int n1, n2, Sum;
     
-    printf("Enter two integers: ");
+    printf("Enter two integers:");
 
     // two integers entered by user are stored using scanf()
     scanf("%d %d", &n1, &n2);
@@ -12,17 +12,17 @@ int main()
     Sum = n1 + n2;
 	
 	if (n1>=n2)
-		printf("Increasing order: %d , %d\n", n2, n1);
+		printf("\nIncreasing order: %d , %d\n\n", n2, n1);
 	else
-		printf("Increasing order: %d , %d\n", n1, n2);
+		printf("\nIncreasing order: %d , %d\n\n", n1, n2);
 
     // display output     
-    printf("%d + %d = %d\n", n1, n2, Sum);
+    printf("%d + %d = %d\n\n", n1, n2, Sum);
 
     // check if the sum is even or odd, 
     // and print result to screen
-    if(Sum % 2 == 0) printf("%d is even\n", Sum);
-    else printf("%d is odd\n", Sum);
+    if(Sum % 2 == 0) printf("%d is even\n\n", Sum);
+    else printf("%d is odd\n\n", Sum);
 
     return 0;
 }
